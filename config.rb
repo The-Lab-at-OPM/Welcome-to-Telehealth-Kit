ad8cf4445f8121c749a224b3b72cc062f1da0d6d
Ana <anamonroe@gmail.com>
2018-11-27 11:19:23 -0500

    Content updates
    Added Quickstart using personal tablet.

--- config.rb
+++ config.rb
@@ -1,4 +1,5 @@
 # Activate and configure extensions
+activate :livereload
 # https://middlemanapp.com/advanced/configuration/#configuring-extensions
 activate :autoprefixer do |prefix|
   prefix.browsers = "last 2 versions"
